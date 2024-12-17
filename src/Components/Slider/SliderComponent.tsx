8d1528a8fce3d2021922602f3e6f31b533202715
Ernesto Carlos <erneskrlos@gmail.com>
2024-12-17 10:11:35 -0300

    Implementation component swiper

---
+++ src/Components/Slider/SliderComponent.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
+import { Navigation, Pagination } from 'swiper/modules';
+import { Grid2 } from '@mui/material';
+
+const SwiperComppnent = () => {
+    return (
+        <Grid2 sx={{ width: '100%', height: '100%' }}>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                modules={[Navigation, Pagination]}
+                navigation
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide>
+                    Slide 1 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+            </Swiper>
+        </Grid2>
+    );
+  };
+  
+  export default SwiperComppnent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/Components/Header"
+import SwiperComppnent from "@/Components/Slider/SliderComponent";
+
 
 export default function Home() {
   return (
     <>
       <Header/>
       <main>
+        <SwiperComppnent />
       </main>
       <footer>
-        
       </footer>
     </>
   );
