2b5a035cac6d069988b0c2bf6d681ea3b65ae736
Ernesto Carlos <erneskrlos@gmail.com>
2024-12-25 21:27:26 -0300

    Implement Step by step to get curse

---
+++ src/Components/Papers/PaperComponent.tsx
@@ -0,0 +1,79 @@
+import { Paper, Theme, useMediaQuery } from '@mui/material'
+import React from 'react'
+import Title from '../Titles/Title'
+
+
+interface PaperComponentProps {
+  theme: Theme,
+  title: string,
+  description: string,
+  step: string
+}
+
+export default function PaperComponent({ theme, title, description, step }: PaperComponentProps) {
+  const isMobil = useMediaQuery("(max-width: 768px)")
+  // const isTable = useMediaQuery("(max-width: 920px)")
+  const screamMedia = useMediaQuery("(min-width: 920px) and (max-width: 1540px)")
+  return (
+    <Paper
+      sx={{
+        height: '100%',
+        width: '264px',
+        backgroundColor: theme.palette.background.default,
+        columnGap: 4
+      }}
+    >
+      <article>
+        <header style={{ height: '80px', margin: '24px 24px 14px 24px' }}>
+          <Title
+            text={title}
+            variant='h3'
+            component={'h3'}
+            style={{
+              fontFamily: '"Quicksand", sans-serif',
+              fontSize: isMobil ? '32px' : screamMedia ? '26px' : '32px',
+              color: theme.palette.text.secondary,
+              fontWeight: '600',
+              textAlign: isMobil ? 'center': 'left',
+              lineHeight: isMobil ? '40px' : screamMedia ? '30px' : '40px',
+              width: '100%',
+              textWrap: 'wrap',
+            }} 
+          />
+        </header>
+        <Title
+            text={description}
+            variant='body1'
+            component={'p'}
+            style={{
+              fontFamily: '"Quicksand", sans-serif',
+              fontSize: isMobil ? '18px' : screamMedia ? '12px' : '18px',
+              color: theme.palette.text.secondary,
+              fontWeight: '400',
+              textAlign: isMobil ? 'center': 'left',
+              width: '100%',
+              textWrap: 'wrap',
+              padding: '0px 24px 30px 24px',
+              height: '160px'
+            }} 
+          />
+        <footer style={{ margin: '24px' }}>
+          <Title
+            text={step}
+            variant='body1'
+            component={'p'}
+            style={{
+              fontFamily: '"Quicksand", sans-serif',
+              fontSize: isMobil ? '16px' : screamMedia ? '12px' : '16px',
+              color: theme.palette.text.secondary,
+              fontWeight: '400',
+              textAlign: isMobil ? 'center': 'left',
+              width: '100%',
+              textWrap: 'wrap',
+            }} 
+          />
+        </footer>
+      </article>
+    </Paper>
+  )
+}
--- src/app/StepByStep/page.tsx
+++ src/app/StepByStep/page.tsx
@@ -4,6 +4,7 @@ import HeadSectionComponent from '@/Components/HeadSection'
 
 import { Grid2, useTheme } from '@mui/material'
 import ImageComponet from '@/Components/Image/ImageComponent'
+import PaperComponent from '@/Components/Papers/PaperComponent'
 
 const styleArticle: React.CSSProperties = {
     marginTop: '80px',
@@ -14,6 +15,33 @@ const styleArticle: React.CSSProperties = {
     width: '100%',
     height: 'auto',
 }
+
+const LIST_STEP_LANGUAGE = [
+    {
+        id: 1,
+        title: 'Primer contacto:',
+        description: 'Una vez que nos contactes, nos comunicaremos contigo para conocer tus objetivos, el idioma que te interesa y tus horarios.',
+        step: 'Paso 1'
+    },
+    {
+        id: 2,
+        title: 'Evaluación inicial:',
+        description: 'Realizaremos una prueba de nivel sencilla, ya sea en línea o con una entrevista corta para colocarte en el curso adecuado para ti.',
+        step: 'Paso 2'
+    },
+    {
+        id: 3,
+        title: 'Propuesta:',
+        description: 'Con la información que obtengamos de ti, te recomendaremos el plan que mejor se adapte a tus necesidades.',
+        step: 'Paso 3'
+    },
+    {
+        id: 4,
+        title: 'Inicio del curso:',
+        description: 'Cuando comiences tu curso, tendrás todo listo y haremos un seguimiento continuo de tu avance.',
+        step: 'Paso 4'
+    }
+]
 
 export default function StepByStepSection() {
     const theme = useTheme()
@@ -29,7 +57,9 @@ export default function StepByStepSection() {
         </Grid2>
         <Grid2 sx={{ display:'grid', gridTemplateColumns: '322px 1fr 322px'}}>
             <Grid2 sx={{ 
-                
+                position: 'relative',
+                top: '295px',
+                left: '66px'
             }} >
                 <ImageComponet
                   src='/resources/images/young_smiling_woman_working_at_laptop.png'
@@ -39,11 +69,25 @@ export default function StepByStepSection() {
                   priority={false} 
                 />
             </Grid2>
-            <Grid2 sx={{ }}>
-                
+            <Grid2 container justifyContent={'center'} spacing={2} sx={{  }}>
+                {
+                    LIST_STEP_LANGUAGE.map((lsl, index) => {
+                        return (
+                            <PaperComponent
+                                key={index}
+                                theme={theme}
+                                title={lsl.title}
+                                description={lsl.description}
+                                step={lsl.step} 
+                            />
+                        )
+                    })
+                }
             </Grid2>
             <Grid2 sx={{ 
-
+                position: 'relative',
+                bottom: '123px',
+                right: '0px'
             }}>
                 <ImageComponet
                   src='/resources/images/young_man_with_laptop_on_chair.png'
