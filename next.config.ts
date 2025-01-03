1ae7f5c7cddb841fdd55e248ebece5d81b1a9323
Ernesto Carlos <erneskrlos@gmail.com>
2025-01-03 11:29:22 -0300

    Update code

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+  // distDir: "dist",
+  trailingSlash: true,
+  assetPrefix: '.',
   /* config options here */
   images: {
     formats: ["image/avif", "image/webp"],
     loader: 'default',
-    // unoptimized: true,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3000'  
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '5500'  
       },
     ],
   },
