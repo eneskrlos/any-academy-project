3ef803001f3a66051dc8e705f2096672f1acff5c
Ernesto Carlos <erneskrlos@gmail.com>
2025-01-30 17:09:29 -0300

    Upload file dist

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  // distDir: "dist",
+  // output: "export",
+  distDir: "dist",
   trailingSlash: true,
   assetPrefix: '.',
   /* config options here */
